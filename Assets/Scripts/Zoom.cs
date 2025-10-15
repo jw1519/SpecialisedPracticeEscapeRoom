50e3e3e314230d05b78338ef1e19770541f47122
Jess <jw1519@student.aru.ac.uk>
2025-10-15 22:51:57 +0100

    added two colliders to table. tried doing zoom with camera

--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -5,18 +5,32 @@ public class Zoom : MonoBehaviour
 {
     public static event Action OnZoom;
     Collider collider;
+    public int zoomLevel = 1;
+    Camera camera => Camera.main;
     private void Awake()
     {
         collider = GetComponent<Collider>();
     }
     private void OnMouseDown()
     {
+        ZoomIn();
+    }
+    public void ZoomIn()
+    {
         if (collider != null)
         {
-            Debug.Log("Zooming in");
-            collider.enabled = false;
+            collider.isTrigger = false;
         }
-        Camera.main.orthographicSize = 1;
+        Debug.Log("Zooming in");
+        camera.transform.position = new Vector3(0, .86f, -.4f);
         OnZoom?.Invoke();
+    }
+    public void ZoomOut()
+    {
+        if (collider != null)
+        {
+            collider.isTrigger = true;
+        }
+        Debug.Log("Zooming out");
     }
 }
