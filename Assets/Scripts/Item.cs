bbf06ddae2fcf16251370e11b0156669ecd81e4f
Jess <jw1519@student.aru.ac.uk>
2025-10-19 23:30:06 +0100

    made item script

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    public int itemID;
+    private void OnMouseDown()
+    {
+        //put item in invent
+        Debug.Log("Item picked up");
+    }
+}
