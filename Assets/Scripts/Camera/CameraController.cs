86e8af5827624dd36ba07eb42c9b565c6bf33f38
Jess <jw1519@student.aru.ac.uk>
2025-11-04 12:44:29 +0000

    added flashlight and organised files

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,7 +4,6 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     public int movementSpeed;
-    Transform rotator;
     CharacterController characterController;
 
     private void Awake()
