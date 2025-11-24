112be2302cf3c9948ce5427a5e37b6667d4f78cc
jw1519 <jw1519@student.aru.ac.uk>
2025-11-24 13:38:13 +0000

    added xoom script to cabinet and a way to open the doors

---
+++ Assets/Scripts/CabinetDoor.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CabinetDoor : MonoBehaviour
+{
+    public DoorType doorType;
+    public Transform leftDoor;
+    public Transform rightDoor;
+    bool isopen = false;
+    public bool canOpen;
+    private void OnMouseDown()
+    {
+        if (isopen) return;
+        if (doorType == DoorType.right)
+        {
+            OpenRightDoor();
+        }
+        else
+            OpenLeftDoor();
+    }
+    public void OpenRightDoor()
+    {
+        rightDoor.localRotation = Quaternion.Euler(0, -90, 0);
+        isopen = true;
+    }
+    public void OpenLeftDoor()
+    {
+        leftDoor.localRotation = Quaternion.Euler(0, 90, 0);
+        isopen = true;
+    }
+    public enum DoorType
+    {
+        right, left,
+    }
+}
